a3d87fb5f5426eb4d57abe9052fdec4d0e8f6f31
jamie-mcg <jamie.mcgowan@mtkresearch.com>
2023-03-08 22:51:07 +0800

    Added diagonal Q method to FishLeg linear layer.

--- src/optim/FishLeg/fishleg_layers.py
+++ src/optim/FishLeg/fishleg_layers.py
@@ -89,7 +89,7 @@ class FishLinear(nn.Linear, FishModule):
         super(FishLinear, self).__init__(
             in_features, out_features, bias, device=device, dtype=dtype
         )
-        
+
         self._layer_name = "Linear"
         self.fishleg_aux = ParameterDict(
             {
@@ -99,7 +99,7 @@ class FishLinear(nn.Linear, FishModule):
         )
         mask_L = torch.tril(torch.ones_like(self.fishleg_aux["L"])).to(device)
         self.fishleg_aux["L"].register_hook(get_zero_grad_hook(mask_L))
-        
+
         mask_R = torch.triu(torch.ones_like(self.fishleg_aux["R"])).to(device)
         self.fishleg_aux["R"].register_hook(get_zero_grad_hook(mask_R))
 
@@ -125,36 +125,26 @@ class FishLinear(nn.Linear, FishModule):
         z = torch.linalg.multi_dot((R, R.T, u, L.T, L))
         return (z[:, :-1], z[:, -1])
 
-    def Qg(self) -> Tuple[Tensor, Tensor]:
-        """ Speed up Qg product, when batch size is smaller than parameter size.
-            By chain rule:
-                
-            .. math::
-                        DW_i = g_i\hat{a}^T_{i-1}
-            where :math:`DW_i` is gradient of parameter of the ith layer, :math:`g_i` is 
-            gradient w.r.t output of ith layer and :math:`\hat{a}_i` is input to ith layer,
-            and output of (i-1)th layer.
+    def diagQ(self) -> Tensor:
+        """The Q matrix defines the inverse fisher approximation as below:
+
+        .. math::
+                    Q_l = (R_lR_l^T \otimes L_lL_l^T)
+
+        where :math:`l` denotes the l-th layer. The matrix :math:`R_l` has size
+        :math:`(N_{l-1} + 1) \\times (N_{l-1} + 1)` while the matrix :math:`L_l` has
+        size :math:`N_l \\times N_l`. The auxiliarary parameters :math:`\lambda`
+        are represented by the matrices :math:`L_l, R_l`.
+
+        The diagonal of this matrix is therefore calculated by relacing the
+
+        .. math::
+                    Q_l = (R_l \circ R_l \otimes L_l \circ L_l)
+
+        where :math:`\circ` is the Hadamard operation and :math:`\otimes` remains as
+        the Kronecker product.
+
         """
-        
-        L = self.fishleg_aux["L"]
-        R = self.fishleg_aux["R"]
-        lft = torch.linalg.multi_dot((R.T, R, self._g))
-        rgt = torch.linalg.multi_dot((self._a, L, L.T))
-        z = lft@rgt
-        return (z[:, :-1], z[:, -1])
-
-    def save_layer_input(self, input_: list[Tensor]) -> None:
-        a = input_[0].to(self.device).clone()
-        a = a.view(-1, a.size(-1))
-        if self.bias is not None:
-            a = torch.cat([a, a.new_ones((*a.shape[:-1], 1))], dim=-1)
-        self._a = a
-
-    def save_layer_grad_output(
-        self, 
-        grad_output: tuple[Tensor,...],
-    ) -> None:
-        g = grad_output[0].to(self.device)
-        g = g.view(-1, g.size(-1))
-        self._g = g.T
-
+        L = torch.sqrt(self.fishleg_aux["scale"]) * self.fishleg_aux["L"]
+        R = torch.sqrt(self.fishleg_aux["scale"]) * self.fishleg_aux["R"]
+        return torch.kron((L * L), (R * R))
