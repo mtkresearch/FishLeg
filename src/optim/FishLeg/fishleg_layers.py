8fb9c821bb7145f97838b3fdd790931748265ce2
jamie-mcg <jamie.mcgowan@mtkresearch.com>
2023-03-18 00:09:06 +0800

    Fixed bug with zero grad hooks.

--- src/optim/FishLeg/fishleg_layers.py
+++ src/optim/FishLeg/fishleg_layers.py
@@ -101,10 +101,10 @@ class FishLinear(nn.Linear, FishModule):
                 "R": Parameter(torch.eye(out_features)),
             }
         )
-        mask_L = torch.triu(torch.ones_like(self.fishleg_aux["L"])).to(device)
-        self.fishleg_aux["L"].register_hook(get_zero_grad_hook(mask_L))
-        mask_R = torch.triu(torch.ones_like(self.fishleg_aux["R"])).to(device)
-        self.fishleg_aux["R"].register_hook(get_zero_grad_hook(mask_R))
+        # mask_L = torch.triu(torch.ones_like(self.fishleg_aux["L"])).to(device)
+        # self.fishleg_aux["L"].register_hook(get_zero_grad_hook(mask_L))
+        # mask_R = torch.triu(torch.ones_like(self.fishleg_aux["R"])).to(device)
+        # self.fishleg_aux["R"].register_hook(get_zero_grad_hook(mask_R))
 
         self.order = ["weight", "bias"]
 
