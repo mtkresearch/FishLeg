Fixed bug in softmax.

src/optim/FishLeg/fishleg_likelihood.py
@@ -191,7 +191,8 @@ class SoftMaxLikelihood(FishLikelihood):
 
     def nll(sef, preds: torch.Tensor, observations: torch.Tensor) -> torch.Tensor:
         logits = log_softmax(preds, dim=1)
-        return -torch.mean(torch.sum(logits * observations, dim=1))
+        obs_one_hot = one_hot(observations, num_classes=logits.shape[-1])
+        return -torch.mean(torch.sum(logits * obs_one_hot, dim=1))
 
     def draw(self, preds: torch.Tensor) -> torch.Tensor:
         logits = torch.log(preds)