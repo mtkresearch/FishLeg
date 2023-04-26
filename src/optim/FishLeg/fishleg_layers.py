a57b0364b116917c9a10d5da5b4ad5decc73b145
R. Xia <rx220@cblgpu07.eng.cam.ac.uk>
2023-04-26 19:54:35 +0100

    fix shape error of diagQ

--- src/optim/FishLeg/fishleg_layers.py
+++ src/optim/FishLeg/fishleg_layers.py
@@ -236,7 +236,7 @@ class FishLinear(nn.Linear, FishModule):
         """
         L = self.fishleg_aux["L"]
         R = self.fishleg_aux["R"]
-        diag = torch.kron(torch.sum(L * L, dim=0), torch.sum(R * R, dim=1))
+        diag = torch.kron(torch.sum(L * L, dim=1), torch.sum(R * R, dim=0))
         diag = diag * \
                 torch.square(self.fishleg_aux["D"].T).reshape(-1) * \
                 torch.square(self.fishleg_aux["A"].T).reshape(-1)
